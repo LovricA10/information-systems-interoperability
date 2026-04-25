64432728df62992c4f52dc48ec34521938efea20
LovricA10 <lovric.andrej5@gmail.com>
2026-04-25 10:55:34 +0200

    initalize project

---
+++ src/main/java/hr/algebra/iis/IisProjectApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.iis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IisProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IisProjectApplication.class, args);
+    }
+
+}
